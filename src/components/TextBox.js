08afbcbfd5d8c60a400d4df1d21b9ccac9acc54b
Chanissa Trithipkaiwanpon <chanissat@Chanissas-MacBook-Pro-2.local>
2019-04-25 18:28:00 +0700

    pang

---
+++ src/components/TextBox.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import HPBar from './HPBar';
+
+const Container = styled.div`
+    position: absolute;
+    z-index: 2;
+    width: 400px;
+    height: 130px;
+    background-color: rgb(255,254,222);
+    border-radius: 20px 10px 20px 10px;
+    border: 5px solid rgb(60,69,30);
+    box-shadow: 10px 13px 1px -2px rgb(86,103,91);
+    font-size: 25px;
+    padding: 15px;
+`
+const Tail = styled.div`
+    content: '';
+    position: absolute;
+    left: 394px;
+    top: 85px;
+    width: 0;
+    height: 0;
+    z-index: 1;
+    border: 55px solid transparent;
+    border-left-color: rgb(86,103,91);
+    border-right: 0;
+    border-bottom: 0;
+`
+
+const TextBox = ({className, name, hp, lv }) => {
+    const [currentHP, setCurrentHP] = useState(hp)
+    const [damage,setDamage] = useState(0)
+    const Damage = (dmg) => {
+        const remainingHP = currentHP - damage < 0 ? 0 : currentHP - damage
+        setCurrentHP(remainingHP)
+        setDamage(dmg)
+        // console.log(currentHP,damage)
+    }
+    return (
+        <div className={className}>
+            {className}
+            <Container><div className="d-flex justify-content-between">
+            <button onClick={()=>{Damage(20)}}>20</button>
+            <div>{name}</div><div>Lv.{lv}</div></div><HPBar hp={currentHP} maxHP={hp} /><div className="d-flex justify-content-end">{currentHP}/{hp}</div></Container>
+            <Tail />
+        </div>
+
+    );
+}
+
+export default TextBox;
--- src/fight/index.js
+++ src/fight/index.js
@@ -1,9 +1,10 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Particles from "react-particles-js";
 import { CSSTransition } from 'react-transition-group';
 import data from './data.json';
 import Sound from "react-sound";
+import TextBox from '../components/TextBox';
 const Screen = styled.div`
   background: url('./bg-battle.png') no-repeat center center fixed;
   background-size: cover;
@@ -19,60 +20,52 @@ const Pokeball = styled.img`
 const Pokemon = styled.img`
 `
 
-const MyPokemon = ({pokemon,showMyPokemon,setBallShow}) => {
-  console.log('dsfds',pokemon);
+const MyPokemon = ({ pokemon, showMyPokemon, setBallShow }) => {
+  console.log('dsfds', pokemon);
   return (
     <span>
-    <CSSTransition 
+      <CSSTransition
         in={showMyPokemon}
         timeout={320}
         classNames="pokemon"
         unmountOnExit
         onEnter={() => setBallShow(false)}>
 
-      <Pokemon src={pokemon.url} className="pokeme" alt="" />
+        <Pokemon src={pokemon.url} className="pokeme" alt="" />
       </CSSTransition>
-      <CSSTransition 
+      <CSSTransition
         in={showMyPokemon}
         timeout={320}
         classNames="pokemon"
         unmountOnExit
-        >
-
-      <div className="pokemon-detail ">
-        <div>HP: {pokemon.hp}</div>
-        <div>LV: {pokemon.lv}</div>
-      </div>
+      >
+        <TextBox  className="pokemon-detail" name={pokemon.name} hp={pokemon.hp} lv={pokemon.lv} />
       </CSSTransition>
 
-      
+
     </span>
   )
 }
 
-const EnemyPokemon =({pokemon,showEnemyPokemon,setBallShow}) => {
+const EnemyPokemon = ({ pokemon, showEnemyPokemon, setBallShow }) => {
   return (
     <span>
-    <CSSTransition 
+      <CSSTransition
         in={showEnemyPokemon}
         timeout={300}
         classNames="pokemon"
         unmountOnExit
         onEnter={() => setBallShow(false)}>
 
-      <Pokemon src={pokemon.url} className="pokeenemy" alt="" />
+        <Pokemon src={pokemon.url} className="pokeenemy" alt="" />
       </CSSTransition>
-      <CSSTransition 
+      <CSSTransition
         in={showEnemyPokemon}
         timeout={320}
         classNames="pokemon"
         unmountOnExit
-        >
-
-      <div className="pokemon-detail-enemy ">
-        <div>HP : {pokemon.hp}</div>
-        <div>LV: {pokemon.lv}</div>
-      </div>
+      >
+        <TextBox className="enemy-detail" name={pokemon.name} hp={2000} lv={pokemon.lv} />
       </CSSTransition>
 
     </span>
@@ -81,15 +74,15 @@ const EnemyPokemon =({pokemon,showEnemyPokemon,setBallShow}) => {
 
 
 const Battle = () => {
-  const {my_pokemon,enemy} = data
+  const { my_pokemon, enemy } = data
   const [sound, setSound] = useState(Sound.status.PLAYING);
-  const [popOut,setPopOut] = useState(Sound.status.STOPPED)
-  const [myPokemon,setMyPokemon] = useState(my_pokemon[0])
-  const [enemyPokemon,setEnemyPokemon] = useState(enemy[0])
-  const [ballMeShow,setBallMeShow] = useState(false)
-  const [ballEnemyShow,setBallEnemyShow] = useState(false)
-  const [showMyPokemon,setShowMyPokemon] = useState(false)
-  const [showEnemyPokemon,setShowEnemyPokemon] = useState(false)
+  const [popOut, setPopOut] = useState(Sound.status.STOPPED)
+  const [myPokemon, setMyPokemon] = useState(my_pokemon[0])
+  const [enemyPokemon, setEnemyPokemon] = useState(enemy[0])
+  const [ballMeShow, setBallMeShow] = useState(false)
+  const [ballEnemyShow, setBallEnemyShow] = useState(false)
+  const [showMyPokemon, setShowMyPokemon] = useState(false)
+  const [showEnemyPokemon, setShowEnemyPokemon] = useState(false)
   const selectMyPokemon = (num) => {
     setBallMeShow(true)
     setTimeout(() => {
@@ -104,7 +97,7 @@ const Battle = () => {
       setEnemyPokemon(enemy[0])
       setShowEnemyPokemon(true)
       setPopOut(Sound.status.PLAYING);
-      
+
     }, 3000);
   }
   const deSelectMyPokemon = () => {
@@ -114,22 +107,23 @@ const Battle = () => {
     setShowEnemyPokemon(false)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     selectMyPokemon(1)
     selectEnemyPokemon(1)
-  },[])
+  }, [])
   return (
     <Screen className="animated fadeInn">
-    
-    <Sound url="/battle.mp4" playStatus={sound} autoLoad={true} />
-    <Sound url="/popout.mp3" playStatus={popOut} onFinishedPlaying={()=>{setPopOut(Sound.status.STOPPED)}} autoLoad={true}/>
-    <button onClick={()=>{deSelectMyPokemon()}}>me Die</button>
-    <button onClick={()=>{selectMyPokemon(2)}}>me222 Die</button>
-    <Pokeball className={(ballMeShow ? "bounce" : "fadeOut")+" me"} src="./pokeball.png" alt=""/>
-    <Pokeball className={(ballEnemyShow ? "bounce" : "fadeOut")+" enemy"} src="./pokeball.png" alt=""/>
-    <button onClick={()=>{deSelectEnemyPokemon()} }>enemy Die</button>
-    <MyPokemon pokemon={myPokemon} showMyPokemon={showMyPokemon} setBallShow={setBallMeShow} />
-    <EnemyPokemon pokemon={enemyPokemon} showEnemyPokemon={showEnemyPokemon} setBallShow={setBallEnemyShow} />
+
+      <Sound url="/battle.mp4" playStatus={sound} autoLoad={true} />
+      <Sound url="/popout.mp3" playStatus={popOut} onFinishedPlaying={() => { setPopOut(Sound.status.STOPPED) }} autoLoad={true} />
+      <button onClick={() => { deSelectMyPokemon() }}>me Die</button>
+      <button onClick={() => { selectMyPokemon(2) }}>me222 Die</button>
+      <Pokeball className={(ballMeShow ? "bounce" : "fadeOut") + " me"} src="./pokeball.png" alt="" />
+      <Pokeball className={(ballEnemyShow ? "bounce" : "fadeOut") + " enemy"} src="./pokeball.png" alt="" />
+      <button onClick={() => { deSelectEnemyPokemon() }}>enemy Die</button>
+      <MyPokemon pokemon={myPokemon} showMyPokemon={showMyPokemon} setBallShow={setBallMeShow} />
+      <EnemyPokemon pokemon={enemyPokemon} showEnemyPokemon={showEnemyPokemon} setBallShow={setBallEnemyShow} />
+
     </Screen>
   )
 }
