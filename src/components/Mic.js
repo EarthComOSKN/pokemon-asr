172f44f927109d433485101a7bcdb6c8f0e61419
Nutchanon Ploypray <mrearth25@gmail.com>
2019-05-02 01:49:13 +0700

    sdlfmdslmfds

--- src/components/Mic.js
+++ src/components/Mic.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Recorder from 'react-mp3-recorder';
 import styled from 'styled-components';
+import Axios from 'axios';
 
 const Container = styled.div`
 	position: fixed;
@@ -8,13 +9,17 @@ const Container = styled.div`
 	top: 85%;
 `;
 const Mic = () => {
-	const _onRecordingComplete = blob => {
-		console.log('recording', blob);
-		var csvURL = window.URL.createObjectURL(blob);
-		var tempLink = document.createElement('a');
-		tempLink.href = csvURL;
-		tempLink.setAttribute('download', 'test.mp3');
-		tempLink.click();
+	const _onRecordingComplete = async (blob) => {
+		try {
+			console.log('recording', blob);
+			var form_data = new FormData();
+			form_data.append('file', blob);
+			const res = await Axios.post('http://localhost:5000/rec',form_data)
+			console.log(res);
+			
+		} catch (error) {
+			
+		}
 	};
 
 	const _onRecordingError = err => {
